9014cdf282c77ca8755d5e80c6330069e166bb97
mahima-abeyrathne <mahimaabeyrathne@gmail.com>
2024-11-25 20:01:25 +0530

    updated packages

--- src/views/packages.vue
+++ src/views/packages.vue
@@ -20,7 +20,7 @@
         <li>No of Persons: Min - 2 , Max - 6</li>
         <li>Food: Dinner, Breakfast, Lunch </li>
         <li> Activities: 2 activities (your chose)</li>
-        <p><strong>Price:</strong> Rs 7500 Per person for Night</p>
+        <p><strong>Price:</strong> Rs 3500 Per person for Night</p>
         <button type="button" class="a-button"><router-link to="/booking">Book Now</router-link></button>  
     </div>
     <br><br><br>
@@ -30,7 +30,7 @@
         <li>No of Persons: Min - 4 , Max - 16</li>
         <li>Food: Dinner Only</li>
         <li> Activities: all activities (your chose)</li>
-        <p><strong>Price:</strong> 10500 Per person for Night</p>
+        <p><strong>Price:</strong> 4000 Per person for Night</p>
         <button class="a-button"><router-link to="/booking">Book Now</router-link> </button>
     </div>
     <br><br><br>
@@ -40,7 +40,7 @@
         <li>No of Persons: Min - 4 , Max - 16</li>
         <li>Food: Dinner Breakfast Lunch </li>
         <li> Activities: All activities (your chose)</li>
-        <p><strong>Price:</strong> 20500 Per person for Night</p>
+        <p><strong>Price:</strong> 4500 Per person for Night</p>
         <button class="a-button"><router-link to="/booking">Book Now</router-link></button> 
     </div>
 
@@ -49,7 +49,7 @@
     <main>
         <section>
             <h2>Contact Information</h2>
-            <p><strong>Phone:</strong> +947131141 </p>
+            <p><strong>Phone:</strong> +9471831141 </p>
             <p><strong>Email:</strong> demodaravillage@gmail.com</p>
         </section>
 
